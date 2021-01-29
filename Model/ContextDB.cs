465c0b6d2f731b3e6e9980ef83f1621c350ecf91
GHOST <GHOST@ASUS>
2021-01-29 18:19:06 +0300

    БД Коннект

---
+++ Model/ContextDB.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace TestSiteApp.Model
+{
+    class ContextDB : DbContext
+    {
+        public ContextDB():base("DbConnection")
+        {
+
+        }
+
+        public DbSet<Site> Sites { get; set; }
+    }
+}
---
+++ Model/Site.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestSiteApp.Model
+{
+    public class Site
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public DateTime TimeInterval { get; set; }
+    }
+}
